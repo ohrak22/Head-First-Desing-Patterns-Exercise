ca960f7d5cb0eabaa6bc12f93ca5c1766993a0e5
ohrak22gmail <ohrak22@gmail.com>
2017-04-05 14:12:55 +0800

    Observer Pattern
    옵저버 패턴

---
+++ DesingPatterns/WeatherStation/CurrentConditionsDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherStation
+{
+	class CurrentConditionsDisplay : Observer, DisplayElement
+	{
+		private float temperature;
+		private float humidity;
+		private Subject weatherData;
+
+		public CurrentConditionsDisplay(Subject weatherData)
+		{
+			this.weatherData = weatherData;
+			weatherData.registerObserver(this);
+		}
+
+		public void update(float temperature, float humidity, float pressure)
+		{
+			this.temperature = temperature;
+			this.humidity = humidity;
+			display();
+		}
+
+		public void display()
+		{
+			Console.WriteLine("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+		}
+	}
+}
